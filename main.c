547f45319523192402e639205c4b08230469ede5
VernKavee <156408744+VernKavee@users.noreply.github.com>
2024-02-02 08:31:04 +0000

    While for loop

--- main.c
+++ main.c
@@ -10,44 +10,61 @@
 #include "node.h"
 
 int main(int argc, const char * argv[]) {
-    int c=5;
+    int z=5;
     struct node a,b,*head ;
-    a.value = c;
+    a.value = z;
     a.next=&b;
     head=&a;
     b.value=head->value+3;
 
     printf("%d\n", head ->value ); //what value for 5
     printf("%d\n", head ->next->value ); //what value for 8
+    
 /*  Exercise I
     1. Add 1 more than at the end
     2. Add value(11)
     3. Make next become NULL
- */
-    
+*/
+    struct node c;
+    b.next=&c;
+    c.value=head->next->value+3;
+    c.next=NULL;
+    printf("%d\n", head->next->next->value);
+
 /*  Exercise II
         1. Add 1 more than at the begining!!!!
         2. Add value (2)
         
 */
+    struct node d;
+    d.next=&a;
+    d.value=2;
+    head=&d;
+    printf("%d\n",head->value);
+    printf("%d\n",head->next->value);
+    printf("%d\n",head->next->next->value);
+    printf("%d\n",head->next->next->next->value);
+
+        
+    /*  Exercise III Use loop to print everything*/
     typedef struct node* NodePtr;
     NodePtr tmp=head; //add temp value to faciliate
-        
-    /*  Exercise III Use loop to print everything
-        int i,n=5;
-        for(i=0;i<n;i++){
-            printf("%3d", tmp->value);
-          // What is missing???
-        }
-    */
+    int i,n=4;
+    for(i=0;i<n;i++){
+        printf("%3d", tmp->value);
+        tmp=tmp->next;
+    }
+    printf("\n");
+    tmp=head;//reset temp to head
     
-   /*  Exercise IV change to while loop!! (you can use NULL to help)
-       
-         while(){
-            printf("%3d", tmp->value);
-           // What is missing???
-        }
-    */
+    /*  Exercise IV change to while loop!! (you can use NULL to help)*/  
+    while(1){
+        printf("%3d", tmp->value);
+        if(tmp->next == NULL) break;
+        tmp=tmp->next;
+    }
+    printf("\n");
+    tmp=head;//reset temp to head
     
  /*  Exercise V Use malloc to create all nodes, instead of create a struct!!
          //use a loop to help
